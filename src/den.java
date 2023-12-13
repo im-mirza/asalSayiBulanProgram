97455b292c0ce38d0389dc87df9d04b32e8d3ca6
Mirza ismayilov <mirza.ismayilov@bil.omu.edu.tr>
2023-12-13 03:44:08 +0300

    Update AsalSayiYazdiranProgram.java

--- src/den.java
+++ src/den.java
@@ -1,9 +1,22 @@
-public class den {
+public class AsalSayiYazdiranProgram {
     public static void main(String[] args) {
-        boolean a=true;
-        for(int i=0;i<2;i++){
-            a=false;
+        System.out.println("1-100 arasındaki asal sayılar:");
+        // 2'den 100'e kadar olan her sayıyı kontrol et
+        for (int sayi = 2; sayi <= 100; sayi++) {
+            boolean asalMi = true;
+
+            // Sayının 2'den kendisi hariç tüm potansiyel bölenleri kontrol et
+                for (int i = 2; i < sayi; i++) {
+                    if (sayi % i == 0) {
+                        asalMi = false;// Sayı başka bir sayıya bölünüyorsa asal değildir
+                        break;
+                    }
+                }
+
+            // Eğer sayı asalsa ekrana yazdır
+            if (asalMi) {
+                System.out.print(sayi + " ");
+            }
         }
-        System.out.println(a);
     }
 }
